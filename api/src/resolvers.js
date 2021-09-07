1c359bebbab0eea9a6aac76954f5ed2e24039728
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-09-07 20:38:33 +0530

    Modelling db schema with SDL and writing pets query

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -3,21 +3,26 @@
  * the type definitions in your scheama
  */
 
+const { models, db } = require('./db');
+
 module.exports = {
   Query: {
-    
+    pets(_, arguments, context,info) {
+      const { Pet } = context;
+
+      return models.Pet.findMany();
+    },
   },
-  Mutation: {
-    
-  },
+
+  // Mutation:{
+
+  // },
   Pet: {
     img(pet) {
       return pet.type === 'DOG'
         ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
+        : 'http://placekitten.com/300/300';
+    },
   },
-  User: {
-    
-  }
-}
+  // User: {},
+};
